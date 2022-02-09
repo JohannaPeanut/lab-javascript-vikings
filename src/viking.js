e9c10dab81542edb6da1f0efc0f19b8f4787e2ad
JohannaPeanut <johanna.k.michel@posteo.de>
2022-02-09 12:27:43 +0100

    iteration 3 and bonus 4

--- src/viking.js
+++ src/viking.js
@@ -17,6 +17,7 @@ class Viking extends Soldier {
     super(health, strength);
     this.name = name;
   }
+
   receiveDamage(damage) {
     this.health = this.health - damage;
     if (this.health > 0) {
@@ -25,16 +26,61 @@ class Viking extends Soldier {
       return `${this.name} has died in act of combat`;
     }
   }
+
   battleCry() {
     return 'Odin Owns You All!';
   }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(newViking) {
+    this.vikingArmy.push(newViking);
+  }
+  addSaxon(newSaxon) {
+    this.saxonArmy.push(newSaxon);
+  }
+  vikingAttack() {
+    let randomIndexSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    const saxon = this.saxonArmy[randomIndexSaxon];
+    const viking =
+      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    const saxonStatus = saxon.receiveDamage(viking.strength);
+    if (saxon.health <= 0) {
+      this.saxonArmy.splice(randomIndexSaxon, 1);
+    }
+    return saxonStatus;
+  }
+  saxonAttack() {
+    let randomIndexViking = Math.floor(Math.random() * this.vikingArmy.length);
+    const viking = this.vikingArmy[randomIndexViking];
+    const saxon =
+      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    const vikingStatus = viking.receiveDamage(saxon.strength);
+    if (viking.health <= 0) {
+      this.vikingArmy.splice(randomIndexViking, 1);
+    }
+    return vikingStatus;
+  }
+  showStatus() {}
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
