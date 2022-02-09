b0f2855355c5afa6e2d20fafbe120f83d0c1365b
JohannaPeanut <johanna.k.michel@posteo.de>
2022-02-09 13:05:34 +0100

    super bonus iteration 5

--- src/viking.js
+++ src/viking.js
@@ -57,6 +57,7 @@ class War {
   addSaxon(newSaxon) {
     this.saxonArmy.push(newSaxon);
   }
+
   vikingAttack() {
     let randomIndexSaxon = Math.floor(Math.random() * this.saxonArmy.length);
     const saxon = this.saxonArmy[randomIndexSaxon];
@@ -79,7 +80,44 @@ class War {
     }
     return vikingStatus;
   }
-  showStatus() {}
+
+  /*
+
+  // generic .attack method that reveives one argument either vikingAttack or saxonAttack
+
+  attacK(whoAttacks) {
+    if (whoAttacks === vikingAttack) {
+      const aggressorArmy = this.vikingArmy;
+      const attackedArmy = this.saxonArmy;
+    } else if (whoAttacks === saxonAttack) {
+      const aggressorArmy = this.saxonArmy;
+      const attackedArmy = this.vikingArmy;
+    }
+
+    const randomIndexAttacked = Math.floor(Math.random() * attackedArmy.length);
+    const attacked = attackedArmy[randomIndexAttacked];
+
+    const aggressor =
+      aggressorArmy[Math.floor(Math.random() * aggressorArmy.length)]; // does this mutate the array, that it should?? I guess yes, because the variable is still referencing the same array...?
+
+    const attackedStatus = attacked.receiveDamage(aggressor.strength); // same here: does this really mutate the actual object??
+
+    if (attacked.health <= 0) {
+      attackedArmy.splice(randomIndexAttacked, 1);
+    }
+    return attackedStatus;
+  }
+  
+*/
+
+  showStatus() {
+    if (this.vikingArmy.length > 0 && this.saxonArmy.length > 0)
+      return 'Vikings and Saxons are still in the thick of battle.';
+    if (this.vikingArmy.length === 0)
+      return `Saxons have fought for their lives and survived another day...`;
+    if (this.saxonArmy.length === 0)
+      return 'Vikings have won the war of the century!';
+  }
 }
 
 // The following is required to make unit tests work.
